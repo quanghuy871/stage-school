443e96939ea486793693c1ef43d9d515797e25cb
Gia Bao <truongbao250@gmail.com>
2025-06-11 17:09:42 +0700

    mobile

--- components/blocks/title-list-button.js
+++ components/blocks/title-list-button.js
@@ -4,32 +4,33 @@ import parse from "html-react-parser";
 
 const TitleListButton = ({ title, list, button }) => {
   return (
-    <section className="title-list-button">
-      <div className="inner bg-orange w-[1600px] h-[705px] p-7 flex flex-col justify-between ">
-        <div className="w-[1035px] h-[220px] ">
+    <section className="title-list-button bg-orange md:h-[705px] mb:h-[623px] p-4 md:p-7 ">
+      <div className="inner md:h-full mb:h-full lp-7 flex flex-col justify-between ">
+        <div className="md:max-w-[1035px] mb-4">
           {title && (
-            <h2 className="font-dm-sans font-bold text-heading-h3  leading-[44px] tracking[-0,3px]  align-bottom">
+            <h2 className=" font-bold text-heading-h3 leading-[44px]  mb:tracking-[-0.5px] tracking-[-0.3px] text-[20px] md:text-[44px] align-bottom">
               {parse(title)}
             </h2>
           )}
         </div>
 
-        <div className="flex flex-row justify-between w-[1440px] h-[705px] p-7 items-end mt-auto pl-1  ">
-          <div className="">
+        <div className="flex  flex-col-reverse flex-direction md:flex-row justify-between p-7 md:items-end gap-4 mt-auto pl-1">
+          <div className=" flex-wrap gap-2 w-full md:w-[1035px]">
             {list.map((item, index) => (
               <p
-                className="flex items-center w-fit h-fit px-3 py-2 gap-[10px] border border-[#DB6600] rounded-full"
+                className="flex items-center w-fit h-fit px-3 py-2 gap-[10px] border border-[#DB6600] rounded-full text-sm mb:seft-end "
                 key={index}
               >
                 {parse(item)}
               </p>
             ))}
           </div>
-          <div className="flex items-center w-[100px] h-[36px] px-3 py-[7px] gap-[10px] rounded-full bg-primary text-white self-end">
+
+          <div className="flex items-center max-w-[100px] h-[36px] px-3 py-[7px] rounded-full bg-primary md:self-end self-start">
             {button && (
               <Link
                 to={button}
-                className=" w-[76px ] h-[22px] text-paragraph-p1 font-elza-text w-[400] leading-[22px] text-[#FF970F] "
+                className="text-paragraph-p1 leading-[22px] text-[#FF970F] text-sm whitespace-nowrap"
               >
                 {parse(button?.text)}
               </Link>
