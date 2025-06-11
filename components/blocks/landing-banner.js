1916c072da44374b7e0320db86ed65298af6ec84
KhanhNgocNe <khanhngoc04032000@gmail.com>
2025-06-11 13:35:13 +0700

    wip: style landing banner

--- components/blocks/landing-banner.js
+++ components/blocks/landing-banner.js
@@ -5,20 +5,28 @@ import parse from "html-react-parser";
 const LandingBanner = ({ title, buttons }) => {
 
     return (
-        <section className="landing-banner">
-            <div className="inner">
-                <div className="">
-                    {title && <h2>{parse(title)}</h2>}
-                    <div>
-                        {buttons.map((button, index) => (
-                            <Link key={index} to={button}>
-                                {parse(button?.text)}
-                            </Link>
-                        ))}
-                    </div>
-                </div>
+      <section className="landing-banner h-[850px] bg-primary">
+        <div className="inner h-full flex items-end md:justify-end ">
+          <div className="flex flex-col gap-6 md:max-w-[683px] max-w-[315px] pb-11">
+            {title && (
+              <h2 className="md:text-heading-h1 text-heading-h4 font-[700] text-secondary tracking-[-2px]">
+                {parse(title)}
+              </h2>
+            )}
+            <div className="flex">
+              {buttons.map((button, index) => (
+                <Link
+                  key={index}
+                  to={button}
+                  className="bg-secondary px-3 py-[7px] rounded-full"
+                >
+                  {parse(button?.text)}
+                </Link>
+              ))}
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     );
 };
 
