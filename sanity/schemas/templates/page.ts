f3dfc0e1ae9cf900496a3abe738da30998da8f9e
KhanhNgocNe <khanhngoc04032000@gmail.com>
2025-06-13 14:38:02 +0700

    update: resolve conflict

--- sanity/schemas/templates/page.ts
+++ sanity/schemas/templates/page.ts
@@ -19,63 +19,58 @@ const page = {
             };
         },
     },
-    prepare(selection) {
-      const { title, isDraft } = selection;
-      const status = isDraft.startsWith("drafts.") ? "Draft" : "Published";
-      return {
-        title,
-        subtitle: status,
-      };
-    },
-  },
-  groups: [
-    {
-      title: "SEO",
-      name: "seo",
-    },
-    {
-      title: "Main",
-      name: "main",
-      default: true,
-    },
-  ],
-  fields: [
-    {
-      name: "seo",
-      title: "Seo",
-      type: "customSeoMetaFields",
-      group: "seo",
-    },
-    {
-      name: "title",
-      title: "Title",
-      type: "string",
-      group: "main",
-    },
-    {
-      name: "slug",
-      type: "slug",
-      title: "Slug",
-      options: { source: "title" },
-      group: "main",
-    },
-    {
-      name: "content",
-      title: "Blocks",
-      type: "array",
-      group: "main",
-      description: "This is where you can add custom blocks.",
-      of: [
-        { type: "landingBanner" },
-        { type: "titleContentButton" },
-        { type: "titleListButton" },
-        { type: "titleFourColumn" },
-        { type: "cta" },
-        { type: "titleContentButtonImage" },
-        { type: "titleAnimation" },
-      ],
-    },
-  ],
+    groups: [
+        {
+            title: "SEO",
+            name: "seo",
+        },
+        {
+            title: "Main",
+            name: "main",
+            default: true,
+        },
+    ],
+    fields: [
+        {
+            name: "seo",
+            title: "Seo",
+            type: "customSeoMetaFields",
+            group: "seo",
+        },
+        {
+            name: "title",
+            title: "Title",
+            type: "string",
+            group: "main",
+        },
+        {
+            name: "slug",
+            type: "slug",
+            title: "Slug",
+            options: { source: "title" },
+            group: "main",
+        },
+        {
+            name: "content",
+            title: "Blocks",
+            type: "array",
+            group: "main",
+            description: "This is where you can add custom blocks.",
+            of: [
+                { type: "landingBanner" },
+                { type: "titleContentButton" },
+                { type: "titleListButton" },
+                { type: "titleFourColumn" },
+                { type: "cta" },
+                { type: "titleContentButtonImage" },
+                { type: "titleAnimation" },
+                { type: "faq" },
+                { type: "imageTitleContentButton" },
+                { type: "doubleImage" },
+                { type: "courseIntro" },
+            ],
+        },
+    ],
 };
 
 export default page;
